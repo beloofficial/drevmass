d093af5f75254418585bf380c404b9208066db84
amantay <amantay.orynbayev@paybox.money>
2023-06-11 18:48:04 +0600

    admin products

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
-                        <div class="card-header">Восстановить пароль</div>
+                        <div class="card-header">Войти</div>
                         <div class="card-body">
 
                             <form action="/admin/login" method="post">
